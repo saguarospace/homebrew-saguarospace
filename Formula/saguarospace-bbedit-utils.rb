af3869cb31292035097a4d7a34970bdf730e94c3
Christopher Green <cgreen@saguaro.space>
2021-12-06 07:00:09 -0700

    Updates saguarospace-bbedit-utils.rb

--- Formula/saguarospace-bbedit-utils.rb
+++ Formula/saguarospace-bbedit-utils.rb
@@ -2,7 +2,7 @@ class SaguarospaceBbeditUtils < Formula
   desc "Saguaro Space BBEdit Utilities"
   homepage ""
   url "https://github.com/saguarospace/saguarospace-bbedit-utils/releases/download/v0.0.4/saguarospace-bbedit-utils.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "1eb80ebd812352d8ea12435924bef4e19e54f18ee0f8b6e67315b36ee398bee8"
   license "MIT"
 
   def install
